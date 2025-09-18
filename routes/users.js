04d45ff7009aa7d63642b62b50851414072f8289
Luiz Rodrigues <lrodrigues@ifsp.edu.br>
2025-09-18 17:49:41 -0300

    create user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../config/database');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', async function (req, res, next) {
+  try {
+    const { name, email, password } = req.body;
+
+    const [result] = await db.execute(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, password]
+    );
+
+    res.status(201).json({ userId: result.insertId });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
